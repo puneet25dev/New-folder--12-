97c51597e3c184dabcc61977d67ef2d020d26d76
Puneet Kumar <kpuneetoff@gmail.com>
2025-08-23 09:54:21 +0530

    Popular Exams

--- script.js
+++ script.js
@@ -9,3 +9,20 @@ bar.addEventListener("click", () => {
     side_bar.style.display = isSidebarVisible ? "flex" : "none";
 });
 
+
+// Exams Tab button fetching
+
+let exam_tabs=document.querySelectorAll(".exam_tabs");
+
+// exam_tabs.forEach(el => console.log(el));
+
+exam_tabs.forEach(el => {
+    let buttons = el.querySelectorAll("button");
+    buttons.forEach(btn => btn.addEventListener("click", () => {
+        buttons.forEach(b => {b.style.backgroundColor = "",b.style.color="black"});
+        
+        btn.style.backgroundColor = "purple";
+        btn.style.color = "white";
+    }));
+});
+
