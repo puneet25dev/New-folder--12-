a35c703fbc31628ff6af6d07a63522a70387f970
Puneet Kumar <kpuneetoff@gmail.com>
2025-08-24 17:58:13 +0530

    SSC and Bank exam card

--- script.js
+++ script.js
@@ -14,7 +14,8 @@ bar.addEventListener("click", () => {
 
 let exam_tabs=document.querySelectorAll(".exam_tabs");
 
-// exam_tabs.forEach(el => console.log(el));
+exam_tabs.forEach(el => console.log(el));
+
 
 exam_tabs.forEach(el => {
     let buttons = el.querySelectorAll("button");
@@ -37,6 +38,30 @@ let get_start_btn=document.querySelector(".get_start_btn");
 get_start_btn.addEventListener("click",()=>{
     window.location.href="signup.html"
 })
+
+
+let ssc_exams = document.querySelector(".ssc_exams");
+let ssc_exams_list = document.querySelector(".ssc_exams_list");
+
+ssc_exams.addEventListener("click",()=>{
+    if(ssc_exams_list){
+        ssc_exams_list.style.transform="translateX(0%)";
+        banking_exams_list.style.transform="translateX(150%)";
+    }
+})
+
+let banking_exams_list=document.querySelector(".banking_exams_list");
+let banking_exams=document.querySelector(".banking_exams");
+
+banking_exams.addEventListener("click",()=>{
+    if(banking_exams_list){
+        ssc_exams_list.style.transform="translateX(150%)";
+        banking_exams_list.style.transform="translateX(0%)";
+    }
+})
+
+
+
 
 
 
